cedb4520aeb975c0060aab912de827a12ffc5f60
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-01-04 16:11:53 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+a = Number(prompt("Enter any table number",5));
+b = Number(prompt("Enter table length",10))
+
+for(i = 1; i <= b; i++){
+    document.write(a + " x " + i + " = " +  a*i + "<br />")
+}
